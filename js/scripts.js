55e74c8faf86f664d4bf131fc749962b6c78be65
Amin Nateghi <aminnateghi10@gmail.com>
2022-01-26 17:45:09 +0330

    footer

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,18 @@
 	})
 
 	$('.owl-carousel').owlCarousel({
-		loop:true,
-		margin:10,
-		nav:true,
-		responsive:{
-			0:{
-				items:1
+		loop: true,
+		margin: 10,
+		nav: true,
+		responsive: {
+			0: {
+				items: 1
 			},
-			600:{
-				items:1
+			600: {
+				items: 1
 			},
-			1000:{
-				items:1
+			1000: {
+				items: 1
 			}
 		}
 	})
